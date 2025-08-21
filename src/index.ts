4ce9455e0df3c841e1ff5888b2a788af14b603e3
Martin Eriksson <martineriksson999@gmail.com>
2025-08-21 14:21:55 +0200

    add User interface and types folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import "dotenv/config" // oneliner for configuration
 import { closeDB, runDB } from "./db/database.js"
+import type { User } from "./types/User.js";
 
 const app = express()
 const port: number = Number(process.env.PORT) || 3000;
@@ -8,6 +9,15 @@ const port: number = Number(process.env.PORT) || 3000;
 app.get("/", (request, response) => {
 response.status(200).send({ message: "Hello world!" })
 })
+
+app.post("/user", (req, res) => {
+  const newUser: User = {
+    id: 1,
+    name: "Benny",
+    email: "benny65@gmail.com",
+  };
+  res.status(201).json({ message: "Användare skapad!", user: newUser });
+});
 
 app.listen(port, "0.0.0.0", ()=>{
     console.log("Listen on port" + port)
