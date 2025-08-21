a6d7f6a579f9561e65b32c0b441fea23cc74dcaf
Martin Eriksson <martineriksson999@gmail.com>
2025-08-21 10:48:37 +0200

    Modified host and port

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from "express"
 
 const app = express()
-const port: number = 3000
+const port: number = Number(process.env.PORT) || 3000;
 
 app.get("/", (request, response) => {
 response.send("Hello world!")
 })
 
-app.listen("/", ()=>{
-    console.log("Hello World!")
+app.listen(port, "0.0.0.0", ()=>{
+    console.log("Listen on port" + port)
 })
 
