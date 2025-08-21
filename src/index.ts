746a21d53764f99478a32b82433d6e7bd88bafa1
Martin Eriksson <martineriksson999@gmail.com>
2025-08-21 14:56:20 +0200

    Modified User.ts, database.ts and index.ts so users can be added to mongodb database

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,34 @@
-import express from "express"
+import express, {type Request, type Response} from "express"
 import "dotenv/config" // oneliner for configuration
-import { closeDB, runDB } from "./db/database.js"
+import { closeDB, getDB, runDB } from "./db/database.js"
 import type { User } from "./types/User.js";
 
 const app = express()
 const port: number = Number(process.env.PORT) || 3000;
 
+app.use(express.json())
+
 app.get("/", (request, response) => {
 response.status(200).send({ message: "Hello world!" })
 })
 
-app.post("/user", (req, res) => {
-  const newUser: User = {
-    id: 1,
-    name: "Benny",
-    email: "benny65@gmail.com",
-  };
-  res.status(201).json({ message: "Användare skapad!", user: newUser });
+app.post("/user", async(req: Request, res: Response) => {
+    const {name, email} = req.body
+
+    try {
+    const db = getDB() // the endpoint "/user" calls getDB() to establish the db connection
+    const result = await db.collection("users").insertOne({ name, email }) // inserts a new doc (row in table) into the users collection
+ 
+    const newUser: User = {
+      id: result.insertedId.toString(), // converts generated MongoDB id from ObjectId to string
+      name,
+      email,
+    }
+    res.status(201).send(newUser) // returns the new user with generated 'id' from MongoDB
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ message: "Failed to create user" })
+  }
 });
 
 app.listen(port, "0.0.0.0", ()=>{
